d0e1cbb87a60449caa7f6ac0a1986e18b47d1317
IrinaZherebtsova <irinazherebtsova1994@gmail.com>
2024-06-20 16:42:45 -1000

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Irina Zherebtsova on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
