35c6331a8b9daa2cc7f7880a7f55c811adba80d9
IrinaZherebtsova <irinazherebtsova1994@gmail.com>
2024-06-20 17:37:19 -1000

    Creating a project

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Irina Zherebtsova on 6/20/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Irina Zherebtsova on 6/20/24.
+//
+
+import Foundation
